cf730b95495147c43779b69a1f8486dfa0a1faba
Zhao Jingchen <dracupid@gmail.com>
2016-01-31 14:23:31 +0800

    fix

--- zsh.sh
+++ zsh.sh
@@ -3,7 +3,7 @@
 sh `dirname $0`/touch_tool.sh zsh
 
 # install oh-my-zsh
-$ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # install https://github.com/zsh-users/zsh-syntax-highlighting
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH}/custom/plugins/zsh-syntax-highlighting
